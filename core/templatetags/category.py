9f4329af821dd56dccb5ded096dd9875ddcc76af
godstime01 <youngestkid01@gmail.com>
2024-04-01 18:20:22 +0100

    more edit fixed optional section

--- core/templatetags/category.py
+++ core/templatetags/category.py
@@ -15,13 +15,43 @@ def category_icon(category):
     elif category.lower() == 'saving':
         imgSrc = '/static/assets/icons/saving.svg'
     elif category.lower() == 'subscription':
-        imgSrc = '/static/assets/icons/subscription.svg'
+        imgSrc = '/static/assets/icons/supscription.svg'
     elif category.lower() == 'hobbies':
         imgSrc = '/static/assets/icons/ocio.svg'
     elif category.lower() == 'medication':
         imgSrc = '/static/assets/icons/medication.svg'
+    else:
+        imgSrc = '/static/assets/icons/Various.svg'
 
     
     print(imgSrc)
     
     return {'src': imgSrc}
+
+
+@register.inclusion_tag('partials/optionals.html')
+def render_optional():
+    subscriptions = [
+        {
+            'name': 'Netflix',
+            'url': '/static/assets/subscription/netflix.svg' 
+        },
+        {
+            'name': 'Spotify',
+            'url': '/static/assets/subscription/spotify.svg'
+        },
+        {
+            'name': 'amazon',
+            'url': '/static/assets/subscription/amazon.svg'
+        },
+        {
+            'name': 'google',
+            'url': '/static/assets/subscription/google.svg'
+        },
+        {
+            'name': 'facebok',
+            'url': '/static/assets/subscription/facebook.svg'
+        }
+    ]
+
+    return {'subscriptions': subscriptions}
